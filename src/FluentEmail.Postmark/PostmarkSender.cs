ba2c0838609505ac1cd2df68485b95ebfecea7c0
Georg Jung <git@gjung.com>
2020-04-13 20:27:53 +0300

    Add library code skeleton

---
+++ src/FluentEmail.Postmark/PostmarkSender.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FluentEmail.Postmark
+{
+    /// <summary>
+    /// A FluentEmail ISender implementation that uses postmark to send emails.
+    /// </summary>
+    public class PostmarkSender
+    {
+    }
+}
